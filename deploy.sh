2bd6f3b796e97fb9a06d8d266d8c86357eae303c
Zeyuan On Hadoop <ferrishu3886@gmail.com>
2016-11-26 18:23:24 +0800

    Fix auto deploy process

--- deploy.sh
+++ deploy.sh
@@ -15,11 +15,11 @@ echo "$TRAVIS_PULL_REQUEST"
 echo "$TRAVIS_BRANCH"
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    doCompile
-    exit 0
-fi
+# if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "SOURCE_BRANCH" ]; then
+#     echo "Skipping deploy; just doing a build."
+#     doCompile
+#     exit 0
+# fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
