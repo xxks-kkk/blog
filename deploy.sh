86d880459d5685121efb11ad1daed163b2e6c06d
Zeyuan On Hadoop <ferrishu3886@gmail.com>
2016-11-26 19:07:30 +0800

    Fix auto deploy process

--- deploy.sh
+++ deploy.sh
@@ -60,9 +60,9 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in id_rsa_travis.pub.enc -out id_rsa_travis.pub -d
-chmod 600 deploy_key
+chmod 600 id_rsa_travis.pub
 eval `ssh-agent -s`
-ssh-add deploy_key
+ssh-add id_rsa_travis.pub
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
